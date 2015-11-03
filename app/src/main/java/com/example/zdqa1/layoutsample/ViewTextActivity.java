83cf025e0544d2d59da0e43bfd3f4b9d929f1e57
Sphynx <229860255@qq.com>
2015-11-03 15:28:41 +0800

    android studio  commint

---
+++ app/src/main/java/com/example/zdqa1/layoutsample/ViewTextActivity.java
@@ -0,0 +1,37 @@
+package com.example.zdqa1.layoutsample;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.widget.TextView;
+
+public class ViewTextActivity extends AppCompatActivity {
+
+    private TextView mlg;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_view_text);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+//        Intent intent = getIntent();
+//        String message = intent.getStringExtra(LoginActivity.LOGINED_MESSAGE);
+//        mlg=(TextView) findViewById(R.id.logined);
+//        mlg.setText(message);
+    }
+
+}
